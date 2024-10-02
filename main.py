0e968613978a2ca9a8800d721e0e67cdcc14079e
Aniket <aniket.modi117@protonmail.com>
2024-10-02 20:37:05 +0530

    minor updt

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ st.title('🏗️ Q&A on Vectors ')
 
 disclaimer = "Answer"
 
-with st.expander("Q 1.	At what angle three forces of equal magnitude may act so that their resultant is equal to zero."):
+with st.expander(
+        "Q 1.	At what angle three forces of equal magnitude may act so that their resultant is equal to zero."):
     st.info("120 degrees")
 
 with st.expander("Q 2.	Can the resultant of three unequal forces be equal to zero ?"):
@@ -25,7 +26,7 @@ with st.expander("Q 6.	Can a east – ward directed force be balanced by a north
 with st.expander("Q 7.	Can the resultant of two forces 10 N and 6 N be equal to"
                  "(a) 3N 	(b) 4N 	(c) 8N 	(d) 16 N   (e) 18 N"):
     st.info("The resultant of 10 N and 6 N would lie between (10 + 6) N and (10 - 6) N so it can be"
-             " 4 N, 8 N, and 16 N.")
+            " 4 N, 8 N, and 16 N.")
 
 with st.expander("Q 8.	In the above problem what will be the minimum and maximum value of the resultant."):
     st.info("4 N and 16 N")
@@ -72,37 +73,48 @@ with st.expander("Q 17.	A man ran 2.0 kilometer towards east, then making a perp
                  " Calculate his total displacement."):
     st.info("The man's total displacement is approximately 6.02 km in a direction 4.8° north of east")
 
-with st.expander("Q 18.	The co-ordinates of the initial and final point of a given vector are respectively (2, 3) and (10, 6). Then calculate the magnitude of the vector."):
-    st.write(disclaimer)
+with st.expander("Q 18.	The co-ordinates of the initial and final point of a given vector are"
+                 " respectively (2, 3) and (10, 9). Then calculate the magnitude of the vector."):
+    st.info("10")
 
-with st.expander("Q 19.	There are two vectors whose magnitude are respectively 2m and 4m the angle between them is 30o. Calculate (1) scalar product of the two vectors and (2) the magnitude of their vector products."):
-    st.write(disclaimer)
+with st.expander("Q 19.	There are two vectors whose magnitude are respectively 2m and 4m the"
+                 " angle between them is 30o. Calculate (1) scalar product of the two vectors"
+                 " and (2) the magnitude of their vector products."):
+    st.info("scalar prod = 4 * sqrt(3); magnitude of cross prod = 4")
 
-with st.expander("Q 20.	Find the components of a vector A which makes equal angles with the x, y and z axis. "):
-    st.write(disclaimer)
+with st.expander("Q 20.	Find the components of a unit vector A which makes equal angles with the x,"
+                 " y and z axis. "):
+    st.info("1 / sqrt(3)")
 
-with st.expander("Q 21.	A body of mass 5 kg moves from position 1 metre to a position 8 metre due to a force 10 Newton. Calculate the work done."):
-    st.write(disclaimer)
+with st.expander("Q 21.	A body of mass 5 kg moves from position 1 metre to a position 8 metre"
+                 " due to a force 10 Newton. Calculate the work done."):
+    st.info("70 joule")
 
-with st.expander("Q 22.	Three equal forces each of value 1 newton acts along the side BC, CD and DE of a regular hexagon ABCDEF. Calculate the magnitude of their resultant."):
-    st.write(disclaimer)
+with st.expander("Q 22.	Three equal forces each of value 1 newton acts along the side BC, CD"
+                 " and DE of a regular hexagon ABCDEF. Calculate the magnitude of their resultant."):
+    st.info("2 newton")
 
-with st.expander("Q 23.	If the co-ordinate of a moving particle are given by x = at2, and y = bt2, the calculate the speed of the particle."):
-    st.write(disclaimer)
+with st.expander("Q 23.	If the co-ordinate of a moving particle are given by x = at2, and"
+                 " y = bt2, the calculate the speed of the particle."):
+    st.info("sqrt((4at*at)+(4bt*bt))")
 
-with st.expander("Q 24.	Calculate the displacement, when we finally lie down on our bed in the night since the time we rose form the same bed in the morning."):
-    st.write(disclaimer)
+with st.expander("Q 24.	Calculate the displacement, when we finally lie down on our bed"
+                 " in the night since the time we rose form the same bed in the morning."):
+    st.info("Zero")
 
-with st.expander("Q 25.	M and N are two sites few kilometers apart on a perfectly flat plane road. A girl takes off from M in a helicopter circles around, and then lands at N; while on the other hand a man walks from M to N; compare their displacements."):
-    st.write(disclaimer)
+with st.expander("Q 25.	M and N are two sites few kilometers apart on a perfectly flat"
+                 " plane road. A girl takes off from M in a helicopter circles around,"
+                 " and then lands at N; while on the other hand a man walks from M to N;"
+                 " compare their displacements."):
+    st.info("The girl and the man will have the same displacements.")
 
-with st.expander("Q 26.	Is it correct that, “the magnitude of the sum of two vectors must be greater than the magnitude of either vectors ?"):
-    st.write(disclaimer)
+with st.expander("Q 26.	Is it correct that, “the magnitude of the sum of two vectors"
+                 " must be greater than the magnitude of either vectors ?"):
+    st.info("No.")
 
 with st.expander("Q 27.	Is it correct that the component of a vector is a vector ? "):
-    st.write(disclaimer)
+    st.info("No.")
 
-with st.expander("Q 28.	Is it correct to say that if a vector is zero then each of its rectangular components must be zero?"):
-    st.write(disclaimer)
-
-st.balloons()
+with st.expander("Q 28.	Is it correct to say that if a vector is zero then each"
+                 " of its rectangular components must be zero?"):
+    st.info("Ja.")
