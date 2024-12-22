4e742c5b0793b6e34ae0823479d9dc4a035b7ee3
Aniket <aniket.modi117@protonmail.com>
2024-12-22 11:46:28 +0530

    filename chng

--- pages/Rotational Motion.py
+++ pages/Rotational Motion.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.image("img/Moment-of-Inertia-1.PNG")
 
-st.image("img/Moment-of-Inertia-1-ans.PNG")
+st.image("img/Moment-of-Inertia-1-ans.png")
 
 st.write("Answer = I(big) - I(small)")
