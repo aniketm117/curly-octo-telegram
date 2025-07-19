2c475b54920ef59372e58df5618dc60219b7ff6c
Aniket <aniket.modi117@protonmail.com>
2025-07-19 20:23:31 +0530

    comments added in Tickets.py

--- pages/Tickets.py
+++ pages/Tickets.py
@@ -4,6 +4,27 @@ import pandas as pd
 import altair as alt
 from datetime import datetime, timedelta
 
+# df = pd.read_csv("data.csv", sep=",")
+#
+# with col1:
+#     for index, row in df[:4].iterrows():
+#         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+#         st.write(row["role"])
+#         st.image("images/"+row["image"])
+#
+# with col2:
+#     for index, row in df[4:8].iterrows():
+#         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+#         st.write(row["role"])
+#         st.image("images/"+row["image"])
+#
+# with col3:
+#     for index, row in df[8:].iterrows():
+#         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+#         st.write(row["role"])
+#         st.image("images/"+row["image"])
+
+
 st.info('To write a ticket, fill out the form below. Check status or review ticketing analytics using the tabs below.')
 
 # Generate data
